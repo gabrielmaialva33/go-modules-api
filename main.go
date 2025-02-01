1499eca74f21ef784d515bdb94159fbe1103ec57
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-02-01 17:10:31 -0300

    chore: add CLI command execution support
    Introduced argument-based command execution using the `cmd` package. If arguments are passed, the application will execute the corresponding command and exit, otherwise it will proceed to start the server as before. This enhances flexibility and enables easier task automation.

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"os"
+
+	"go-modules-api/cmd"
 	"go-modules-api/config"
 	"go-modules-api/internal/server/http"
 	"go-modules-api/utils"
@@ -17,6 +20,11 @@ func main() {
 	logger.Info("Starting the application")
 
 	config.Load(log)
+
+	if len(os.Args) > 1 {
+		cmd.Execute()
+		return
+	}
 
 	logger.Info("server started", zap.Int("port", config.Env.AppPort))
 	logger.Sugar().Infof("server is running at %s", config.Env.AppHost)
