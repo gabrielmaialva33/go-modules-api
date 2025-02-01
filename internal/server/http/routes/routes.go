0e42ecaa64398603fefca38a40c7507c859f29f8
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-02-01 16:44:05 -0300

    feat: add HTTP server implementation with routing and middleware
    Introduces a Fiber-based HTTP server with CORS support, logging middleware, and two basic routes. The server serves static documentation and redirects the root endpoint to the docs. This setup provides the foundation for future API development.

---
+++ internal/server/http/routes/routes.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"go.uber.org/zap"
+)
+
+func SetupRoutes(app *fiber.App, log *zap.Logger) {
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Redirect("/docs")
+	})
+
+	app.Get("/docs", func(c *fiber.Ctx) error {
+		return c.SendFile("./docs/redoc.html")
+	})
+
+}
---
+++ internal/server/http/server.go
@@ -0,0 +1,46 @@
+package http
+
+import (
+	"fmt"
+
+	"go-modules-api/config"
+	"go-modules-api/internal/server/http/middleware"
+	"go-modules-api/internal/server/http/routes"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"go.uber.org/zap"
+)
+
+type Server struct {
+	App *fiber.App
+	Log *zap.Logger
+}
+
+func NewServer(log *zap.Logger) *Server {
+	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+	app.Use(middleware.RequestLogger(log))
+
+	app.Static("/", "./docs")
+
+	routes.SetupRoutes(app, log)
+
+	return &Server{
+		App: app,
+		Log: log,
+	}
+}
+
+func (s *Server) Start() {
+	port := fmt.Sprintf(":%d", config.Env.AppPort)
+	log := s.Log.Named("server")
+	log.Sugar().Infof("server is running at %s", port)
+	if err := s.App.Listen(port); err != nil {
+		log.Fatal("error while starting server", zap.Error(err))
+	}
+}
