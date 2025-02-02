5f980e7ba3489c37b5f06c8899396b79d4a596e3
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-02-01 21:38:33 -0300

    chore: add custom boolean validator and enhance error details
    Introduced a custom "is_bool" validator for pointer to boolean fields using `reflect`. Enhanced validation errors to include allowed values for "oneof" tag violations by extracting them from parameters.

--- utils/validator.go
+++ utils/validator.go
@@ -2,6 +2,8 @@ package utils
 
 import (
 	"fmt"
+	"reflect"
+	"strings"
 
 	"github.com/go-playground/validator/v10"
 )
@@ -9,9 +11,34 @@ import (
 var validate = validator.New()
 
 type ValidationError struct {
-	Field string `json:"field"`
-	Tag   string `json:"tag"`
-	Value string `json:"value"`
+	Field         string   `json:"field"`
+	Tag           string   `json:"tag"`
+	Value         string   `json:"value"`
+	AllowedValues []string `json:"allowed_values,omitempty"`
+}
+
+// Custom validation function for boolean pointers
+func boolValidator(fl validator.FieldLevel) bool {
+	field := fl.Field()
+	if field.Kind() == reflect.Ptr && !field.IsNil() {
+		_, ok := field.Interface().(*bool)
+		return ok
+	}
+	return true
+}
+
+func init() {
+	err := validate.RegisterValidation("is_bool", boolValidator)
+	if err != nil {
+		fmt.Println("Error registering custom validation:", err)
+	}
+}
+
+func extractAllowedValues(param string) []string {
+	if param != "" {
+		return strings.Fields(param)
+	}
+	return nil
 }
 
 func ValidateStruct(data interface{}) []*ValidationError {
@@ -24,6 +51,11 @@ func ValidateStruct(data interface{}) []*ValidationError {
 				Tag:   err.Tag(),
 				Value: fmt.Sprintf("%v", err.Value()),
 			}
+
+			if err.Tag() == "oneof" {
+				element.AllowedValues = extractAllowedValues(err.Param())
+			}
+
 			errors = append(errors, element)
 		}
 	}
