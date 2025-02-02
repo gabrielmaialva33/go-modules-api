61ed8f7df4f98dbfa4385f9bc3ba12544f095090
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-02-02 16:58:41 -0300

    feat: add support for soft deletes and filter out deleted records
    Introduced a `SoftDelete` method to mark records as deleted without removing them from the database. Updated CRUD operations to utilize a new scope that filters out records marked as deleted. This ensures better data integrity and aligns with soft delete practices.

--- internal/repositories/base_repository.go
+++ internal/repositories/base_repository.go
@@ -1,7 +1,10 @@
 package repositories
 
 import (
+	"time"
+
 	"go-modules-api/internal/models"
+
 	"gorm.io/gorm"
 )
 
@@ -11,6 +14,7 @@ type BaseRepositoryInterface[T any] interface {
 	Create(entity T) error
 	Update(entity T) error
 	Delete(id uint) error
+	SoftDelete(entity T) error
 }
 
 // BaseRepository provides common CRUD operations for any model type that has an ID.
@@ -23,10 +27,15 @@ func NewBaseRepository[T models.IDGetter](db *gorm.DB) *BaseRepository[T] {
 	return &BaseRepository[T]{db: db}
 }
 
+// scopeNotDeleted adds a condition to ignore records where IsDeleted is true.
+func scopeNotDeleted(db *gorm.DB) *gorm.DB {
+	return db.Where("is_deleted = ?", false)
+}
+
 // GetByID retrieves a record by its ID.
 func (r *BaseRepository[T]) GetByID(id uint) (T, error) {
 	var entity T
-	if err := r.db.First(&entity, id).Error; err != nil {
+	if err := r.db.Scopes(scopeNotDeleted).First(&entity, id).Error; err != nil {
 		return entity, err
 	}
 	return entity, nil
@@ -39,13 +48,18 @@ func (r *BaseRepository[T]) Create(entity T) error {
 
 // Update updates an existing record.
 func (r *BaseRepository[T]) Update(entity T) error {
-	return r.db.Where("id = ?", entity.GetID()).Updates(entity).Error
+	return r.db.Scopes(scopeNotDeleted).Where("id = ?", entity.GetID()).Updates(entity).Error
 }
 
 // Delete removes a record from the database by its ID.
 func (r *BaseRepository[T]) Delete(id uint) error {
 	var entity T
-	return r.db.Delete(&entity, id).Error
+	return r.db.Scopes(scopeNotDeleted).Delete(&entity, id).Error
+}
+
+// SoftDelete marks a record as deleted without actually removing it from the database.
+func (r *BaseRepository[T]) SoftDelete(entity T) error {
+	return r.db.Model(entity).Updates(map[string]interface{}{"is_deleted": true, "deleted_at": time.Now()}).Error
 }
 
 // Ensure BaseRepository implements BaseRepositoryInterface.
