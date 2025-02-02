dfd3f3d87169ac821f3e6beedb7d87c3bf607cf7
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-02-01 22:25:44 -0300

    chore: add pagination support for hub clients API
    Introduced `PaginatedHubClientDTO` and updated OpenAPI documentation to include a new endpoint `/api/hub_clients/paginate` for retrieving paginated hub client data. Added query parameters for filtering, sorting, and pagination options along with response metadata to provide detailed pagination info.

--- internal/dto/hub_client_dto.go
+++ internal/dto/hub_client_dto.go
@@ -16,3 +16,12 @@ type GetHubClientDTO struct {
 	SortField string `json:"sort_field" validate:"omitempty,oneof=id name active external_id created_at updated_at"`
 	SortOrder string `json:"sort_order" validate:"omitempty,oneof=asc desc"`
 }
+
+type PaginatedHubClientDTO struct {
+	Page      int    `json:"page" validate:"omitempty,min=1"`
+	PageSize  int    `json:"page_size" validate:"omitempty,min=1,max=100"`
+	Search    string `json:"search" validate:"omitempty"`
+	Active    *bool  `json:"active" validate:"omitempty"`
+	SortField string `json:"sort_field" validate:"omitempty,oneof=id name active external_id created_at updated_at"`
+	SortOrder string `json:"sort_order" validate:"omitempty,oneof=asc desc"`
+}
