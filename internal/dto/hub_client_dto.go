cd01b6e81d83c8e4b67233f61218f5d62699ae42
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-02-01 21:05:30 -0300

    feat: add DTOs for creating and updating Hub clients
    Introduce `CreateHubClientDTO` and `UpdateHubClientDTO` structs to handle data transfer for Hub client creation and updates. These include validation rules to ensure data integrity.

---
+++ internal/dto/hub_client_dto.go
@@ -0,0 +1,11 @@
+package dto
+
+type CreateHubClientDTO struct {
+	Name       string `json:"name" validate:"required,min=3,max=100"`
+	ExternalID uint   `json:"external_id" validate:"required"`
+}
+
+type UpdateHubClientDTO struct {
+	Name       string `json:"name" validate:"min=3,max=100"`
+	ExternalID uint   `json:"external_id"`
+}
